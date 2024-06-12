3f098f06079b2bfb88d844d35bd156241df35d56
Putu Wiranto <leonkmoena@gmail.com>
2024-06-12 15:31:00 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,96 @@
-import bone from "./assets/bone.png";
-
 function App() {
   return (
-    <div className="container justify-center text-center mx-auto">
-      <h1 className="font-custom text-[50px] pt-10 text-center">
-        Introducing Based POPO
-      </h1>
-      <div className="text-center">
-        <h2 className="font-custom2 text-[50px]">pepe's dog</h2>
-      </div>
-      <div className="mt-32">
-        <div className="flex justify-evenly">
-          <h1 className="font-custom bg-green-400 w-[300px] text-[25px] rounded-md p-5">
-            LP BURNED TROUGH APE.STORE
-          </h1>
-          <h1 className="font-custom bg-green-400 w-[300px] text-[25px] rounded-md p-5">
-            TOTAL SUPLY 2,000,000,000
+    <>
+      <div className="section1 container mx-auto">
+        <div className="container flex justify-center items-center pt-[5rem]">
+          <h1 className="font-custom text-black w-[30%] text-justify uppercase text-sm  mb-10 font-bold">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, sit!
+            Autem sapiente nesciunt facilis asperiores illum ullam quas iusto
+            dolorum quaerat, odio labore quo minima nihil. Earum asperiores
+            corrupti repellat. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Delectus earum tempore vero quidem laborum et
+            eveniet eaque dolorem quaerat eius ipsam amet, numquam sequi ea, cum
+            magni esse provident quam?
           </h1>
         </div>
-        <div className="right-0 justify-evenly flex">
-          <h1 className="font-custom bg-green-400 w-[300px] text-[25px] rounded-md p-10 mt-20">
-            TAX 0/0
+        <div className="font-custom flex justify-center  space-x-48">
+          <a
+            href=""
+            className="bg-yellow-500 border-[2px] font-extrabold border-black px-6 py-1 rounded-full hover:bg-green-900 hover:text-white"
+          >
+            TWITTER
+          </a>
+          <a
+            href=""
+            className="bg-yellow-500 border-[2px] border-black py-1 px-6 rounded-full hover:bg-green-900 hover:text-white"
+          >
+            TELEGRAM
+          </a>
+        </div>
+      </div>
+      <div className="section2 container mt-20 ">
+        <div className="flex justify-center">
+          <h1 className="font-custom uppercase w-[30%] text-justify ">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate
+            nostrum itaque iste nobis modi ipsa tenetur aliquam. Maiores
+            assumenda vitae distinctio quia qui ad velit asperiores recusandae!
+            Porro, inventore alias. Lorem ipsum, dolor sit amet consectetur
+            adipisicing elit. Aliquam recusandae similique delectus ipsam id
+            pariatur beatae, ea veniam animi dignissimos odit, ducimus,
+            assumenda tempora minus quibusdam modi magni reprehenderit laborum.
           </h1>
         </div>
       </div>
-      <div className="justify-center flex mt-10">
-        <img src={bone} alt="bone" className="w-[500px]" />
+      <div className="section3 flex flex-col items-center space-y-2 justify-center mt-20 font-custom">
+        <a
+          className="border-[2px] w-[300px] justify-center flex border-black rounded-full bg-yellow-500 hover:bg-green-900 hover:text-white uppercase"
+          href=""
+        >
+          Twitter
+        </a>
+        <a
+          className="border-[2px] w-[300px] justify-center flex border-black rounded-full bg-yellow-500 hover:bg-green-900 hover:text-white uppercase"
+          href=""
+        >
+          Telegram
+        </a>
+        <a
+          className="border-[2px] w-[300px] justify-center flex border-black rounded-full bg-yellow-500 hover:bg-green-900 hover:text-white uppercase"
+          href=""
+        >
+          Instagram
+        </a>
+        <a
+          className="border-[2px] w-[300px] justify-center flex border-black rounded-full bg-yellow-500 hover:bg-green-900 hover:text-white uppercase"
+          href=""
+        >
+          Mail
+        </a>
+        <a
+          className="border-[2px] w-[300px] justify-center flex border-black rounded-full bg-yellow-500 hover:bg-green-900 hover:text-white uppercase"
+          href=""
+        >
+          Basescan
+        </a>
+        <a
+          className="border-[2px] w-[300px] justify-center flex border-black rounded-full bg-yellow-500 hover:bg-green-900 hover:text-white "
+          href=""
+        >
+          Dextools
+        </a>
+        <a
+          className="border-[2px] w-[300px] justify-center flex border-black rounded-full bg-yellow-500 hover:bg-green-900 hover:text-white "
+          href=""
+        >
+          Dexscreener
+        </a>
       </div>
-    </div>
+      <div className="mt-10">
+        <h1 className="font-semibold text-2xl">
+          0x7422f030958Edf9884cE9c46a211029E03eF6da7
+        </h1>
+      </div>
+    </>
   );
 }
 
